93736d2306ff36bbcf5d5f7b017421f44c07ff90
micha <micha@asus>
2024-10-11 19:45:34 +0200

    Initial  Commit.

---
+++ test3/src/main/java/me/michal/project/test3/App.java
@@ -0,0 +1,13 @@
+package me.michal.project.test3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "jebac maven" );
+    }
+}
